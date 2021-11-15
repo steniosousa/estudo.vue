05bea99b7c5eb9fa096010107c004f56da996878
stenio sousa <81321894+steniosousa@users.noreply.github.com>
2021-11-15 16:31:36 -0300

    Add files via upload

---
+++ src/components/teste.vue
@@ -0,0 +1,22 @@
+<template>
+<div><h1>{{ teste1 }}</h1>
+
+<p>teste</p>
+
+</div>
+    
+</template>
+
+<script>
+export default {
+    name:'teste',
+    components:{
+        teste1:String,
+    }
+    
+}
+</script>
+
+<style scoped>
+
+</style>
